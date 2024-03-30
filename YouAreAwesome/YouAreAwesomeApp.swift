7846d6285b4ddca92958361bcaa246afea4a8311
essgeejay <sg.jeffrey@outlook.com>
2024-03-30 20:17:20 +0000

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Shane Jeffrey on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
