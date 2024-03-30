480c8351e03e1cb57b231dea3f481de8f2d362a0
essgeejay <sg.jeffrey@outlook.com>
2024-03-30 20:49:26 +0000

    Ch. 1.1 Xcode Tour & Text + Image Intro (You Are Awesome SwiftUI app)
    What I learned in this lesson:
    
    -Tips for working with Xcode project files
    -Tour of Xcode
    -Views and Modifiers
    -Modify a Text view & an Image view
        - Attribute Inspector
        - Writing code
    -Code completion
    -Dot notation for modifiers & pre-defined
    -Xcode Library
    - Intro to understanding errors
    -Keyboard Shortcuts & Pro Tips

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .padding()
+            Text("Your Are Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .italic()
         }
         .padding()
     }
